d4f07b2dc2da0a24e3c95bee14f814fe79a96d04
Krykl <ana.lopez@DUIO814001.mintel.int>
2015-07-09 17:20:57 -0500

    imte_prov2
    cambio de dt.tie_anio a fi.fit_fecha

--- imte_prov.sql
+++ imte_prov.sql
@@ -9,108 +9,106 @@
 	round(c.p_capacidad_t_instalada::numeric,4) as "Capacidad Tech Instalada"
 from
 
-(SELECT dt.tie_anio AS anio,
+(SELECT  extract(year from fi.fit_fecha) AS anio, 
             ug.ubi_provincia AS provincia,
             ug.ubi_provincia_cod AS codigo,
             fi.fit_valor AS imte,
             di.itd_sk AS sk1
            FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt
+            "TD".fact_indicadores_td fi
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =232
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) i
+          AND fi.fit_fecha = '2014-12-31') i
 
-JOIN (	SELECT ug.ubi_provincia AS provincia,
+JOIN (	SELECT  extract(year from fi.fit_fecha) AS anio,
+		ug.ubi_provincia AS provincia,
                ug.ubi_provincia_cod AS codigo,
 		fi.fit_valor as p_fortaleza_empresarial
-	FROM "TD".dim_indicador_territorio_digitales di,
+		FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
+            "TD".fact_indicadores_td fi	
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =228
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) e
+          AND fi.fit_fecha = '2014-12-31') e
 	  ON(e.codigo = i.codigo)
 	
-JOIN (	SELECT ug.ubi_provincia AS provincia,
+JOIN (	SELECT extract(year from fi.fit_fecha) AS anio,
+		ug.ubi_provincia AS provincia,
                ug.ubi_provincia_cod AS codigo,
 		fi.fit_valor as p_interes_empresas_tic
 	FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
+            "TD".fact_indicadores_td fi	
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =229
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) t
+          AND fi.fit_fecha = '2014-12-31') t
 	  ON(t.codigo = i.codigo)
 
-JOIN (	SELECT ug.ubi_provincia AS provincia,
+JOIN (	SELECT extract(year from fi.fit_fecha) AS anio,
+		ug.ubi_provincia AS provincia,
                ug.ubi_provincia_cod AS codigo,
 		fi.fit_valor as p_uso_tecnologia
 	FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
+            "TD".fact_indicadores_td fi	
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =230
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) u
+          AND fi.fit_fecha = '2014-12-31') u
 	  ON(u.codigo = i.codigo)
 
-JOIN (	SELECT ug.ubi_provincia AS provincia,
+JOIN (	SELECT extract(year from fi.fit_fecha) AS anio,
                ug.ubi_provincia_cod AS codigo,
 		fi.fit_valor as p_capacidad_t_instalada
 	FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
-          WHERE di.itd_sk = fi.fit_itd_sk 
+            "TD".fact_indicadores_td fi
+            WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =231
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) c
+          AND fi.fit_fecha = '2014-12-31') c
 	  ON(c.codigo = i.codigo)
 
-JOIN (	SELECT ug.ubi_provincia AS provincia,
+JOIN (	SELECT extract(year from fi.fit_fecha) AS anio,
+		ug.ubi_provincia AS provincia,
                ug.ubi_provincia_cod AS codigo,
 		fi.fit_valor as idv3
 	FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
+            "TD".fact_indicadores_td fi	
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =159
           AND ug.ubi_parroquia is null
           AND ug.ubi_canton is null
-          AND dt.tie_sk = fi.fit_tie_sk) v
+          AND fi.fit_fecha = '2014-12-31') v
 	  ON(v.codigo = i.codigo)
 
-JOIN (	SELECT  ug.ubi_provincia,
+JOIN (	SELECT  extract(year from fi.fit_fecha) AS anio,
+		ug.ubi_provincia,
 	ug.ubi_provincia_cod AS codigo,
 	sum(fi.fit_valor) AS poblacion 
 	FROM "TD".dim_indicador_territorio_digitales di,
             "TD".dim_ubicacion_geografica ug,
-            "TD".fact_indicadores_td fi,
-            dim_tiempo dt	
+            "TD".fact_indicadores_td fi	
           WHERE di.itd_sk = fi.fit_itd_sk 
           AND ug.ubi_sk = fi.fit_ubi_sk 
           AND di.itd_sk =160
-          AND dt.tie_sk = fi.fit_tie_sk
-          group by ug.ubi_provincia,ug.ubi_provincia_cod) p
+         AND fi.fit_fecha = '2014-12-31'
+          group by fi.fit_fecha, ug.ubi_provincia,ug.ubi_provincia_cod) p
 	  ON(p.codigo = i.codigo)
 	
