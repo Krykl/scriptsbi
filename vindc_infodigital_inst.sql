464cf3ca669afe580e231414a43d2eb6824043ea
Krykl <ana.lopez@DUIO814001.mintel.int>
2015-07-10 15:50:40 -0500

    Índice y ranking de Información digital por Instituciones
    Valores de conexiones a web services y conexiones a portal web, cálculo
    de índice y ranking por institución.

---
+++ vindc_infodigital_inst.sql
@@ -0,0 +1,12 @@
+﻿select 
+año,
+institucion,
+conx_ws,
+conx_pweb,
+round(((know_infod + interop_tram + conx_dinardap + mn_proc_infod + infod_norm_inst + virt_tram + 
+plan_less_tram + 4*log_conx_ws + 4*log_conx_pweb)/15)::numeric,4) as indice,
+
+row_number() OVER (ORDER BY ((know_infod + interop_tram + conx_dinardap + mn_proc_infod + infod_norm_inst + virt_tram + 
+plan_less_tram + 4*log_conx_ws + 4*log_conx_pweb)/15) DESC,know_infod DESC,institucion ASC) as ranking
+
+from "INST".vdata_infod_inst_std
